783fe07e627516c068a870d59800ba9dab87536e
yhwang20 <yhwang20@jh.edu>
2022-10-16 01:13:13 -0700

    Changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "hw4-estore-yhwang20",
-  plugins: [react()]
+  plugins: [react()] 
 })
